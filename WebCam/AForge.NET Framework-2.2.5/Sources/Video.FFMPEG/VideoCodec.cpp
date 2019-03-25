9f15f4b4a890818ad5bae8a0c9aeaf0c9c333676
Paul Young <p.young79@outlook.com>
2019-03-25 15:42:37 +0000

    Update the supported codec types for FFMPEG

--- WebCam/AForge.NET Framework-2.2.5/Sources/Video.FFMPEG/VideoCodec.cpp
+++ WebCam/AForge.NET Framework-2.2.5/Sources/Video.FFMPEG/VideoCodec.cpp
@@ -19,42 +19,176 @@ namespace libffmpeg
 
 int video_codecs[] =
 {
-	libffmpeg::AV_CODEC_ID_MPEG4,
-	libffmpeg::AV_CODEC_ID_WMV1,
-	libffmpeg::AV_CODEC_ID_WMV2,
-	libffmpeg::AV_CODEC_ID_MSMPEG4V2,
-	libffmpeg::AV_CODEC_ID_MSMPEG4V3,
-	libffmpeg::AV_CODEC_ID_H263P,
-	libffmpeg::AV_CODEC_ID_FLV1,
-	libffmpeg::AV_CODEC_ID_MPEG2VIDEO,
-	libffmpeg::AV_CODEC_ID_RAWVIDEO,
-	libffmpeg::AV_CODEC_ID_FFV1,
-	libffmpeg::AV_CODEC_ID_FFVHUFF,
-	libffmpeg::AV_CODEC_ID_H264,
-	libffmpeg::AV_CODEC_ID_H265,
-	libffmpeg::AV_CODEC_ID_THEORA,
-	libffmpeg::AV_CODEC_ID_VP8,
-	libffmpeg::AV_CODEC_ID_VP9,
+	libffmpeg::AV_CODEC_ID_MPEG1VIDEO,      // mpeg1video
+	libffmpeg::AV_CODEC_ID_MPEG2VIDEO,      // mpeg1video
+	libffmpeg::AV_CODEC_ID_H261,            // h261
+	libffmpeg::AV_CODEC_ID_H263,            // h263
+	libffmpeg::AV_CODEC_ID_RV10,            // rv10
+	libffmpeg::AV_CODEC_ID_RV20,            // rv20
+	libffmpeg::AV_CODEC_ID_MJPEG,           // mjpeg
+	libffmpeg::AV_CODEC_ID_LJPEG,           // ljpeg
+	libffmpeg::AV_CODEC_ID_JPEGLS,          // jpegls
+	libffmpeg::AV_CODEC_ID_MPEG4,           // mpeg4
+	libffmpeg::AV_CODEC_ID_RAWVIDEO,        // rawvideo
+	libffmpeg::AV_CODEC_ID_MSMPEG4V2,       // msmpeg4v2
+	libffmpeg::AV_CODEC_ID_MSMPEG4V3,       // msmpeg4v3
+	libffmpeg::AV_CODEC_ID_WMV1,            // wmv1
+	libffmpeg::AV_CODEC_ID_WMV2,            // wmv2
+	libffmpeg::AV_CODEC_ID_H263P,           // h263p
+	libffmpeg::AV_CODEC_ID_FLV1,            // flv1
+	libffmpeg::AV_CODEC_ID_SVQ1,            // svq1
+	libffmpeg::AV_CODEC_ID_DVVIDEO,         // dvvideo
+	libffmpeg::AV_CODEC_ID_HUFFYUV,         // huffyuv
+	libffmpeg::AV_CODEC_ID_H264,            // h264
+	libffmpeg::AV_CODEC_ID_THEORA,          // theora
+	libffmpeg::AV_CODEC_ID_ASV1,            // asv1
+	libffmpeg::AV_CODEC_ID_ASV2,            // asv2
+	libffmpeg::AV_CODEC_ID_FFV1,            // ffv1
+	libffmpeg::AV_CODEC_ID_CLJR,            // cljr
+	libffmpeg::AV_CODEC_ID_ROQ,             // roq
+	libffmpeg::AV_CODEC_ID_CINEPAK,         // cinepak
+	libffmpeg::AV_CODEC_ID_MSVIDEO1,        // msvideo1
+	libffmpeg::AV_CODEC_ID_ZLIB,            // zlib
+	libffmpeg::AV_CODEC_ID_QTRLE,           // qtrle
+	libffmpeg::AV_CODEC_ID_PNG,             // png
+	libffmpeg::AV_CODEC_ID_PPM,             // ppm
+	libffmpeg::AV_CODEC_ID_PBM,             // pbm
+	libffmpeg::AV_CODEC_ID_PGM,             // pgm
+	libffmpeg::AV_CODEC_ID_PGMYUV,          // pgmyuv
+	libffmpeg::AV_CODEC_ID_PAM,             // pam
+	libffmpeg::AV_CODEC_ID_FFVHUFF,         // ffvhuff
+	libffmpeg::AV_CODEC_ID_BMP,             // bmp
+	libffmpeg::AV_CODEC_ID_ZMBV,            // zmbv
+	libffmpeg::AV_CODEC_ID_FLASHSV,         // flashsv
+	libffmpeg::AV_CODEC_ID_JPEG2000,        // jpeg2000
+	libffmpeg::AV_CODEC_ID_TARGA,           // targa
+	libffmpeg::AV_CODEC_ID_TIFF,            // tiff
+	libffmpeg::AV_CODEC_ID_GIF,             // gif
+	libffmpeg::AV_CODEC_ID_DNXHD,           // dnxhd
+	libffmpeg::AV_CODEC_ID_SGI,             // sgi
+	libffmpeg::AV_CODEC_ID_AMV,             // amv
+	libffmpeg::AV_CODEC_ID_PCX,             // pcx
+	libffmpeg::AV_CODEC_ID_SUNRAST,         // sunrast
+	libffmpeg::AV_CODEC_ID_DIRAC,           // dirac
+	libffmpeg::AV_CODEC_ID_V210,            // v210
+	libffmpeg::AV_CODEC_ID_DPX,             // dpx
+	libffmpeg::AV_CODEC_ID_FLASHSV2,        // flashsv2
+	libffmpeg::AV_CODEC_ID_R210,            // r210
+	libffmpeg::AV_CODEC_ID_VP8,             // vp8
+	libffmpeg::AV_CODEC_ID_A64_MULTI,       // a64multi
+	libffmpeg::AV_CODEC_ID_A64_MULTI5,      // a64multi5
+	libffmpeg::AV_CODEC_ID_R10K,            // r10k
+	libffmpeg::AV_CODEC_ID_PRORES,          // prores
+	libffmpeg::AV_CODEC_ID_UTVIDEO,         // utvideo
+	libffmpeg::AV_CODEC_ID_V410,            // v410
+	libffmpeg::AV_CODEC_ID_XWD,             // xwd
+	libffmpeg::AV_CODEC_ID_XBM,             // xbm
+	libffmpeg::AV_CODEC_ID_VP9,             // vp9
+	libffmpeg::AV_CODEC_ID_WEBP,            // webp
+	libffmpeg::AV_CODEC_ID_HEVC,            // hevc
+	libffmpeg::AV_CODEC_ID_H265,            // h265
+	libffmpeg::AV_CODEC_ID_ALIAS_PIX,       // alias_pix
+	libffmpeg::AV_CODEC_ID_HAP,             // hap
+	libffmpeg::AV_CODEC_ID_Y41P,            // y41p
+	libffmpeg::AV_CODEC_ID_AVRP,            // avrp
+	libffmpeg::AV_CODEC_ID_AVUI,            // avui
+	libffmpeg::AV_CODEC_ID_AYUV,            // ayuv
+	libffmpeg::AV_CODEC_ID_V308,            // v308
+	libffmpeg::AV_CODEC_ID_V408,            // v408
+	libffmpeg::AV_CODEC_ID_YUV4,            // yuv4
+	libffmpeg::AV_CODEC_ID_XFACE,           // xface
+	libffmpeg::AV_CODEC_ID_SNOW,            // snow
+	libffmpeg::AV_CODEC_ID_APNG,            // apng
+	libffmpeg::AV_CODEC_ID_MAGICYUV,        // magicyuv
+	libffmpeg::AV_CODEC_ID_AV1,             // av1
+	libffmpeg::AV_CODEC_ID_FITS,            // fits
 };
 
 int pixel_formats[] =
 {
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_MPEG4,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_WMV1,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_WMV2,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_MSMPEG4V2,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_MSMPEG4V3,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_H263P,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_FLV1,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_MPEG2VIDEO,
-	libffmpeg::AV_PIX_FMT_BGR24,    // libffmpeg::AV_CODEC_ID_RAWVIDEO,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_FFV1,
-	libffmpeg::AV_PIX_FMT_RGB32,    // libffmpeg::AV_CODEC_ID_FFVHUFF,
-	libffmpeg::AV_PIX_FMT_YUVJ420P, // libffmpeg::AV_CODEC_ID_H264,
-	libffmpeg::AV_PIX_FMT_YUVJ420P, // libffmpeg::AV_CODEC_ID_H265,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_THEORA,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_VP8,
-	libffmpeg::AV_PIX_FMT_YUV420P,  // libffmpeg::AV_CODEC_ID_VP9,
+	libffmpeg::AV_PIX_FMT_YUV420P,          // mpeg1video
+	libffmpeg::AV_PIX_FMT_YUV420P,          // mpeg2video
+	libffmpeg::AV_PIX_FMT_YUV420P,          // h261
+	libffmpeg::AV_PIX_FMT_YUV420P,          // h263
+	libffmpeg::AV_PIX_FMT_YUV420P,          // rv10
+	libffmpeg::AV_PIX_FMT_YUV420P,          // rv20
+	libffmpeg::AV_PIX_FMT_YUVJ420P,         // mjpeg
+	libffmpeg::AV_PIX_FMT_BGR24,            // ljpeg
+	libffmpeg::AV_PIX_FMT_BGR24,            // jpegls
+	libffmpeg::AV_PIX_FMT_YUV420P,          // mpeg4
+	libffmpeg::AV_PIX_FMT_RGB24,            // rawvideo
+	libffmpeg::AV_PIX_FMT_YUV420P,          // msmpeg4v2
+	libffmpeg::AV_PIX_FMT_YUV420P,          // msmpeg4v3
+	libffmpeg::AV_PIX_FMT_YUV420P,          // wmv1
+	libffmpeg::AV_PIX_FMT_YUV420P,          // wmv2
+	libffmpeg::AV_PIX_FMT_YUV420P,          // h263p
+	libffmpeg::AV_PIX_FMT_YUV420P,          // flv1
+	libffmpeg::AV_PIX_FMT_YUV410P,          // svq1
+	libffmpeg::AV_PIX_FMT_YUV411P,          // dvvideo
+	libffmpeg::AV_PIX_FMT_YUV422P,          // huffyuv
+	libffmpeg::AV_PIX_FMT_YUV420P,          // h264
+	libffmpeg::AV_PIX_FMT_YUV420P,          // theora
+	libffmpeg::AV_PIX_FMT_YUV420P,          // asv1
+	libffmpeg::AV_PIX_FMT_YUV420P,          // asv2
+	libffmpeg::AV_PIX_FMT_YUV420P,          // ffv1
+	libffmpeg::AV_PIX_FMT_YUV411P,          // cljr
+	libffmpeg::AV_PIX_FMT_YUVJ444P,         // roq
+	libffmpeg::AV_PIX_FMT_RGB24,            // cinepak
+	libffmpeg::AV_PIX_FMT_RGB555LE,         // msvideo1
+	libffmpeg::AV_PIX_FMT_BGR24,            // zlib
+	libffmpeg::AV_PIX_FMT_RGB24,            // qtrle
+	libffmpeg::AV_PIX_FMT_RGB24,            // png
+	libffmpeg::AV_PIX_FMT_RGB24,            // ppm
+	libffmpeg::AV_PIX_FMT_MONOWHITE,        // pbm
+	libffmpeg::AV_PIX_FMT_GRAY8,            // pgm
+	libffmpeg::AV_PIX_FMT_YUV420P,          // pgmyuv
+	libffmpeg::AV_PIX_FMT_RGB24,            // pam
+	libffmpeg::AV_PIX_FMT_YUV420P,          // ffvhuff
+	libffmpeg::AV_PIX_FMT_BGRA,             // bmp
+	libffmpeg::AV_PIX_FMT_PAL8,             // zmbv
+	libffmpeg::AV_PIX_FMT_BGR24,            // flashsv
+	libffmpeg::AV_PIX_FMT_RGB24,            // jpeg2000
+	libffmpeg::AV_PIX_FMT_BGR24,            // targa
+	libffmpeg::AV_PIX_FMT_RGB24,            // tiff
+	libffmpeg::AV_PIX_FMT_RGB8,             // gif
+	libffmpeg::AV_PIX_FMT_YUV422P,          // dnxhd
+	libffmpeg::AV_PIX_FMT_RGB24,            // sgi
+	libffmpeg::AV_PIX_FMT_YUVJ420P,         // amv
+	libffmpeg::AV_PIX_FMT_RGB24,            // pcx
+	libffmpeg::AV_PIX_FMT_BGR24,            // sunrast
+	libffmpeg::AV_PIX_FMT_YUV420P,          // dirac
+	libffmpeg::AV_PIX_FMT_YUV422P10LE,      // v210
+	libffmpeg::AV_PIX_FMT_GRAY8,            // dpx
+	libffmpeg::AV_PIX_FMT_BGR24,            // flashsv2
+	libffmpeg::AV_PIX_FMT_RGB48LE,          // r210
+	libffmpeg::AV_PIX_FMT_YUV420P,          // vp8
+	libffmpeg::AV_PIX_FMT_GRAY8,            // a64multi
+	libffmpeg::AV_PIX_FMT_GRAY8,            // a64multi5
+	libffmpeg::AV_PIX_FMT_RGB48LE,          // r10k
+	libffmpeg::AV_PIX_FMT_YUV422P10LE,      // prores
+	libffmpeg::AV_PIX_FMT_GBRP,             // utvideo
+	libffmpeg::AV_PIX_FMT_YUV444P10LE,      // v410
+	libffmpeg::AV_PIX_FMT_BGRA,             // xwd
+	libffmpeg::AV_PIX_FMT_MONOWHITE,        // xbm
+	libffmpeg::AV_PIX_FMT_YUV420P,          // vp9
+	libffmpeg::AV_PIX_FMT_BGRA,             // webp
+	libffmpeg::AV_PIX_FMT_YUV420P,          // hevc
+	libffmpeg::AV_PIX_FMT_YUV420P,          // h265
+	libffmpeg::AV_PIX_FMT_BGR24,            // alias_pix
+	libffmpeg::AV_PIX_FMT_RGBA,             // hap
+	libffmpeg::AV_PIX_FMT_YUV411P,          // y41p
+	libffmpeg::AV_PIX_FMT_RGB48LE,          // avrp
+	libffmpeg::AV_PIX_FMT_UYVY422,          // avui
+	libffmpeg::AV_PIX_FMT_YUVA444P,         // ayuv
+	libffmpeg::AV_PIX_FMT_YUV444P,          // v308
+	libffmpeg::AV_PIX_FMT_YUVA444P,         // v408
+	libffmpeg::AV_PIX_FMT_YUV420P,          // yuv4
+	libffmpeg::AV_PIX_FMT_MONOWHITE,        // xface
+	libffmpeg::AV_PIX_FMT_YUV420P,          // snow
+	libffmpeg::AV_PIX_FMT_RGB24,            // apng
+	libffmpeg::AV_PIX_FMT_GBRP,             // magicyuv
+	libffmpeg::AV_PIX_FMT_YUV420P,          // av1
+	libffmpeg::AV_PIX_FMT_GBRAP16BE,        // fits
 };
 
 int CODECS_COUNT(sizeof(video_codecs) / sizeof(libffmpeg::AVCodecID));
--- WebCam/AForge.NET Framework-2.2.5/Sources/Video.FFMPEG/VideoCodec.h
+++ WebCam/AForge.NET Framework-2.2.5/Sources/Video.FFMPEG/VideoCodec.h
@@ -28,107 +28,426 @@ namespace AForge
 			public enum class VideoCodec
 			{
 				/// <summary>
-				///   Default video codec, which FFmpeg library selects for the specified file format.
+				///   Special codec identifier meaning that the audio codec should be chosen automatically.
 				/// </summary>
-				///
 				Default = -1,
+
+				/// <summary>
+				///   MPEG-1 video.
+				/// </summary>
+				mpeg1video,
+
+				/// <summary>
+				///   MPEG-2 video.
+				/// </summary>
+				mpeg2video,
+
+				/// <summary>
+				///   H.261.
+				/// </summary>
+				h261,
+
+				/// <summary>
+				///   H.263 / H.263-1996, H.263+ / H.263-1998 / H.263 version 2.
+				/// </summary>
+				h263,
+
+				/// <summary>
+				///   RealVideo 1.0.
+				/// </summary>
+				rv10,
+
+				/// <summary>
+				///    RealVideo 2.0.
+				/// </summary>
+				rv20,
+
+				/// <summary>
+				///   Motion JPEG.
+				/// </summary>
+				mjpeg,
+
+				/// <summary>
+				///   Lossless JPEG.
+				/// </summary>
+				ljpeg,
+
+				/// <summary>
+				///   JPEG-LS.
+				/// </summary>
+				jpegls,
 
 				/// <summary>
 				///   MPEG-4 part 2.
 				/// </summary>
-				///
-				MPEG4 = 0,
+				mpeg4,
+
+				/// <summary>
+				///   raw video.
+				/// </summary>
+				rawvideo,
+
+				/// <summary>
+				///   MPEG-4 part 2 Microsoft variant version 2.
+				/// </summary>
+				msmpeg4v2,
+
+				/// <summary>
+				///   MPEG-4 part 2 Microsoft variant version 3.
+				/// </summary>
+				msmpeg4v3,
 
 				/// <summary>
 				///   Windows Media Video 7.
 				/// </summary>
-				///
-				WMV1,
+				wmv1,
 
 				/// <summary>
 				///   Windows Media Video 8.
 				/// </summary>
-				///
-				WMV2,
+				wmv2,
 
 				/// <summary>
-				/// MPEG-4 part 2 Microsoft variant version 2.
+				///   H.263+ / H.263-1998 / H.263 version 2.
 				/// </summary>
-				///
-				MSMPEG4v2,
+				h263p,
 
 				/// <summary>
-				/// MPEG-4 part 2 Microsoft variant version 3.
+				///   LV / Sorenson Spark / Sorenson H.263 (Flash Video).
 				/// </summary>
-				///
-				MSMPEG4v3,
+				flv1,
 
 				/// <summary>
-				/// H.263+ / H.263-1998 / H.263 version 2.
+				///   Sorenson Vector Quantizer 1 / Sorenson Video 1 / SVQ1.
 				/// </summary>
-				///
-				H263P,
+				svq1,
 
 				/// <summary>
-				/// Flash Video (FLV) / Sorenson Spark / Sorenson H.263.
+				///   DV (Digital Video).
 				/// </summary>
-				FLV1,
+				dvvideo,
 
 				/// <summary>
-				/// MPEG-2 part 2.
+				///   HuffYUV.
 				/// </summary>
-				///
-				MPEG2,
+				huffyuv,
 
 				/// <summary>
-				/// Raw (uncompressed) video.
+				///   H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10.
 				/// </summary>
-				///
-				Raw,
+				h264,
 
 				/// <summary>
-				/// FF video codec 1 lossless codec.
+				///   Theora.
 				/// </summary>
-				///
-				FFV1,
+				theora,
 
 				/// <summary>
-				/// FFmpeg's HuffYUV lossless codec.
+				///   ASUS V1.
 				/// </summary>
-				///
-				FFVHUFF,
+				asv1,
 
 				/// <summary>
-				/// H.264/MPEG-4 Part 10.
+				///  ASUS V2.
 				/// </summary>
-				///
-				H264,
+				asv2,
 
 				/// <summary>
-				/// H.265
+				///   FFmpeg video codec #1.
 				/// </summary>
-				///
-				H265,
+				ffv1,
 
 				/// <summary>
-				/// H.264/MPEG-4 Part 10.
+				///   Cirrus Logic AccuPak.
 				/// </summary>
-				///
-				Theora,
+				cljr,
 
 				/// <summary>
-				/// VP-8.
+				///   id RoQ video.
 				/// </summary>
-				///
-				VP8,
+				roq,
 
 				/// <summary>
-				/// VP-9.
+				///   Cinepak.
 				/// </summary>
-				///
-				VP9,
+				cinepak,
+
+				/// <summary>
+				///   Microsoft Video 1.
+				/// </summary>
+				msvideo1,
+
+				/// <summary>
+				///   LCL (LossLess Codec Library) ZLIB.
+				/// </summary>
+				zlib,
+
+				/// <summary>
+				///   QuickTime Animation (RLE) video.
+				/// </summary>
+				qtrle,
+
+				/// <summary>
+				///   PNG (Portable Network Graphics) image.
+				/// </summary>
+				png,
+
+				/// <summary>
+				///   PPM (Portable PixelMap) image.
+				/// </summary>
+				ppm,
+
+				/// <summary>
+				///   PBM (Portable BitMap) image.
+				/// </summary>
+				pbm,
+
+				/// <summary>
+				///   PGM (Portable GrayMap) image.
+				/// </summary>
+				pgm,
+
+				/// <summary>
+				///   PGMYUV (Portable GrayMap YUV) image.
+				/// </summary>
+				pgmyuv,
+
+				/// <summary>
+				///   PAM (Portable AnyMap) image.
+				/// </summary>
+				pam,
+
+				/// <summary>
+				///   Huffyuv FFmpeg variant.
+				/// </summary>
+				ffvhuff,
+
+				/// <summary>
+				///   BMP (Windows and OS/2 bitmap).
+				/// </summary>
+				bmp,
+
+				/// <summary>
+				///    Zip Motion Blocks Video.
+				/// </summary>
+				zmbv,
+
+				/// <summary>
+				///   Flash Screen Video v1.
+				/// </summary>
+				flashsv,
+
+				/// <summary>
+				///   JPEG 2000.
+				/// </summary>
+				jpeg2000,
+
+				/// <summary>
+				///   Truevision Targa image.
+				/// </summary>
+				targa,
+
+				/// <summary>
+				///   TIFF image.
+				/// </summary>
+				tiff,
+
+				/// <summary>
+				///   GIF (Graphics Interchange Format).
+				/// </summary>
+				gif,
+
+				/// <summary>
+				///   VC3/DNxHD.
+				/// </summary>
+				dnxhd,
+
+				/// <summary>
+				///   SGI image.
+				/// </summary>
+				sgi,
+
+				/// <summary>
+				///   AMV Video.
+				/// </summary>
+				amv,
+
+				/// <summary>
+				///   PC Paintbrush PCX image.
+				/// </summary>
+				pcx,
+
+				/// <summary>
+				///   Sun Rasterfile image
+				/// </summary>
+				sunrast,
+
+				/// <summary>
+				///   Dirac.
+				/// </summary>
+				dirac,
+
+				/// <summary>
+				///   Uncompressed 4:2:2 10-bit.
+				/// </summary>
+				v210,
+
+				/// <summary>
+				///   DPX (Digital Picture Exchange) image.
+				/// </summary>
+				dpx,
+
+				/// <summary>
+				///   Flash Screen Video v2.
+				/// </summary>
+				flashsv2,
+
+				/// <summary>
+				///   Uncompressed RGB 10-bit.
+				/// </summary>
+				r210,
+
+				/// <summary>
+				///   On2 VP8.
+				/// </summary>
+				vp8,
+
+				/// <summary>
+				///   Multicolor charset for Commodore 64.
+				/// </summary>
+				a64multi,
+
+				/// <summary>
+				///   Multicolor charset for Commodore 64, extended with 5th color (colram).
+				/// </summary>
+				a64multi5,
+
+				/// <summary>
+				///   AJA Kona 10-bit RGB Codec.
+				/// </summary>
+				r10k,
+
+				/// <summary>
+				///   Apple ProRes (iCodec Pro).
+				/// </summary>
+				prores,
+
+				/// <summary>
+				///   Ut Video.
+				/// </summary>
+				utvideo,
+
+				/// <summary>
+				///    Uncompressed 4:4:4 10-bit.
+				/// </summary>
+				v410,
+
+				/// <summary>
+				///   XWD (X Window Dump) image.
+				/// </summary>
+				xwd,
+
+				/// <summary>
+				///   XBM (X BitMap) image.
+				/// </summary>
+				xbm,
+
+				/// <summary>
+				///   Google VP9.
+				/// </summary>
+				vp9,
+
+				/// <summary>
+				///   WebP.
+				/// </summary>
+				webp,
+
+				/// <summary>
+				///   H.265 / HEVC (High Efficiency Video Coding).
+				/// </summary>
+				hevc,
+
+				/// <summary>
+				///   H.265 / HEVC (High Efficiency Video Coding).
+				///   This is an alias for <see cref="hevc"/>.
+				/// </summary>
+				h265,
+
+				/// <summary>
+				///   Alias/Wavefront PIX image.
+				/// </summary>
+				alias_pix,
+
+				/// <summary>
+				///   Vidvox Hap.
+				/// </summary>
+				hap,
+
+				/// <summary>
+				///   Uncompressed YUV 4:1:1 12-bit.
+				/// </summary>
+				y41p,
+
+				/// <summary>
+				///   Avid 1:1 10-bit RGB Packer.
+				/// </summary>
+				avrp,
+
+				/// <summary>
+				///   Avid Meridien Uncompressed.
+				/// </summary>
+				avui,
+
+				/// <summary>
+				///   Uncompressed packed MS 4:4:4:4.
+				/// </summary>
+				ayuv,
+
+				/// <summary>
+				///   Uncompressed packed 4:4:4.
+				/// </summary>
+				v308,
+
+				/// <summary>
+				///   Uncompressed packed QT 4:4:4:4.
+				/// </summary>
+				v408,
+
+				/// <summary>
+				///   Uncompressed packed 4:2:0.
+				/// </summary>
+				yuv4,
+
+				/// <summary>
+				///   X-face image.
+				/// </summary>
+				xface,
+
+				/// <summary>
+				///   Snow.
+				/// </summary>
+				snow,
+
+				/// <summary>
+				///   APNG (Animated Portable Network Graphics) image.
+				/// </summary>
+				apng,
+
+				/// <summary>
+				///   MagicYUV video.
+				/// </summary>
+				magicyuv,
+
+				/// <summary>
+				///   Alliance for Open Media AV1.
+				/// </summary>
+				av1,
+
+				/// <summary>
+				///   FITS (Flexible Image Transport System).
+				/// </summary>
+				fits,
 			};
-
 		}
 	}
 }
